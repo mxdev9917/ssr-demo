870e71aedb53d75178df925796fd748e40405fc2
mxdev9917 <140176546+mxdev9917@users.noreply.github.com>
2025-02-12 20:11:17 +0700

    create project

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div className="h-screen w-full bg-[#171927]">
+        <nav className="flex items-center relative w-full h-20  bg-[#27293c]">
+          <div className="flex gap-4 sm :gap-3 lg:gap-24  w-full pl-5 ">
+            <p className="text-xl sm:text-4xl lg:text-5xl  font-bold">
+              <span className="text-white">FEFA</span>
+              <span className="text-[#fce700]">PRESS</span>
+            </p>
+            <input type="text" className=" w-40 sm:w-60 lg:w-96 bg-[#171927] text-gray-400 text-[10px] sm:text-sm lg:text-lg font-bold rounded-md pl-2 sm:pl-5" placeholder="SEARCH SPORTS AND STATS" />
+          </div>
+          <div className="lg:flex lg:items-center absolute w-[1000px] right-0 h-96 lg:h-10 top-[85px] lg:top-5  md:h-96 bg-white ">fdfdf</div>
+        </nav>
+      </div>
+    </>
+  )
+}
+
+export default App
+
+
+
+// absolute 
